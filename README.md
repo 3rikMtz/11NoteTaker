7d38c57c03bc06eb9b41e8ea58ccc5c9fc6a88b1
3rikMtz <eri0305@hotmail.com>
2023-07-23 14:11:17 -0600

    one

---
+++ README.md
@@ -0,0 +1,42 @@
+Here is a markdown (.md) file of your README with additional emojis:
+
+```markdown
+# :notebook_with_decorative_cover: Express.js Note Taker 
+
+## :pencil2: Description
+This is a note taking application built with Express.js that can be used to write and save notes. It uses an Express.js back end and saves and retrieves note data from a JSON file.
+
+## :books: User Story
+```
+AS A small business owner
+I WANT to be able to write and save notes
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+```
+
+## :sparkles: Features
+- :bookmark_tabs: Notes listing in a left-hand column.
+- :memo: Empty fields on the right-hand side for a new note's title and text.
+- :floppy_disk: Save icon appears in the navigation at the top of the page when a new note is being created.
+- :point_right: Click on an existing note in the list in the left-hand column, and that note appears in the right-hand column.
+- :pen: Click on the Write icon in the navigation at the top of the page to be presented with empty fields to enter a new note title and the note's text.
+
+## :rocket: Getting Started
+To use the application, you need to have Node.js and npm installed on your machine.
+
+1. :arrow_down: Clone the repository
+2. :wrench: Install the dependencies using `npm install`
+3. :arrow_forward: Start the server using `npm start`
+4. :globe_with_meridians: Access the application on `localhost:3000` from your browser.
+
+## :earth_americas: Deployed Application
+The application is deployed on Heroku. You can access it [here](<Heroku Link>).
+
+## :scroll: License
+This project is licensed under the terms of the MIT license.
+
+## :question: Questions
+For any questions, please feel free to reach out to me on:
+- GitHub: [@<your-github-username>](https://github.com/<your-github-username>)
+- Email: <your-email>
+```
+Remember to replace `<Heroku Link>`, `<your-github-username>`, and `<your-email>` with your actual Heroku link for the deployed application, your GitHub username, and your email address respectively.
---
+++ server.js
@@ -0,0 +1,150 @@
+const express = require('express');
+const fs = require('fs');
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+const dbJson = require('./Develop/db/db.json');
+const uuid = require('./Develop/helpers/id_gen');
+
+
+const app = express();
+app.use(express.static('Develop/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+);
+
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+
+    fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
+      if(err) {
+        throw err;
+      } 
+
+      res.json(JSON.parse(data));
+
+
+
+    })
+
+  /*  fs.readFile("./Develop/db/db.json", (err, data) => {
+      if(err) {
+        throw err;
+      } 
+      console.log("No Encoding Data: ", data);
+
+      res.json(data);
+    })
+    */
+});
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title, 
+            text,
+            note_id: uuid(),
+        }
+
+        fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+            if(err) {
+                console.log(err);
+            } else {
+                
+                const parsedNote = JSON.parse(data);
+                parsedNote.push(newNote);
+
+                fs.writeFile('./Develop/db/db.json', JSON.stringify(parsedNote, null, 4), (writeErr) =>
+                    writeErr ? console.error(writeErr) : console.info('Succesfully added note!')
+                );
+            };
+        })
+
+        const response = {
+            status: 'succes',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error posting new note');
+    }
+
+    
+});
+
+app.get('/api/notes/:note_id', (req, res) => {
+    if(req.params.note_id) {
+        console.info(`${req.method} request received to get a single note`);
+        const noteId = req.params.note_id;
+
+        fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+            if(err) {
+                console.error(err);
+            } else {
+                const parsedData = JSON.parse(data);
+                const foundNote = parsedData.find(note => note.note_id === noteId);
+
+                if(foundNote) {
+                    console.log(foundNote);
+                    res.status(200).json(foundNote);
+                } else {
+                    console.log('Note not found');
+                }
+            }
+        })
+
+    } else {
+        res.status(400).json('Insert a valid note id');
+    }
+});
+
+app.delete('/api/notes/:note_id', (req, res) => {
+    console.log("Request Parameters Object:¨", req.params);
+
+    if(req.params.note_id) {
+        console.info(`${req.method} received to delete a note`);
+        const noteId = req.params.note_id;
+
+        fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+            
+            if(err) {
+                console.error(err);
+            } else {
+                let parsedData = JSON.parse(data);
+                const filteredData = parsedData.filter(note => note.note_id !== noteId);
+
+                if(filteredData.length !== parsedData.length) {
+                    fs.writeFile('./Develop/db/db.json', JSON.stringify(filteredData), 'utf8', (err) => {
+                        if(err) {
+                            console.error(err);
+                        } else {
+                            console.info('Note deleted succesfully');
+                            res.status(200).json(filteredData);
+                        }
+                    });
+                }
+
+            }
+        })
+
+
+    } else {
+        console.log('Insert a valid note id');
+    }
+    
+});
+
+app.listen(PORT, () => 
+    console.info(`Server listening at http://localhost:${PORT}`)
+);
